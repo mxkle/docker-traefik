667cf08555b5bd64b26a3b9bf4437ba5416939be
root <root@ps01.virtual.lan>
2018-02-24 16:36:26 +0100

    tested python script [ci skip]

--- test/test.py
+++ test/test.py
@@ -1,1 +1,14 @@
-subprocess.check_output("curl -s -H Host:whoami.docker.localhost http://127.0.0.1")
+import subprocess
+
+def check_load_balancing():
+    whoami1 = subprocess.check_output(["curl","-s","-H","Host:whoami.docker.localhost","http://127.0.0.1"]).split(' ')[1]
+    whoami2 = subprocess.check_output(["curl","-s","-H","Host:whoami.docker.localhost","http://127.0.0.1"]).split(' ')[1]
+
+    if whoami1 != whoami2:
+        print "success!"
+        return 1
+    else:
+        print "fail"
+        return 0
+
+check_load_balancing()
